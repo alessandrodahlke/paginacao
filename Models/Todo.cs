d7e50a70a1c5e6ba5b32153d24341d21bbac282b
Alessandro Dahlke <alessandro.dahlke@meta.com.br>
2022-06-09 10:28:03 -0300

    Initial Commit

---
+++ Models/Todo.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Paginacao.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Done { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
